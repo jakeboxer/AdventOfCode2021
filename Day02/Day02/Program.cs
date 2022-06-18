0d9d7bdb4e12de66952a54ce7b17bde2830d81cb
Jake Card <jacob.boxer@gmail.com>
2022-06-18 16:34:55 -0400

    Day 2

---
+++ Day02/Day02/Program.cs
@@ -0,0 +1,22 @@
+﻿var x = 0;
+var y = 0;
+
+foreach (var line in File.ReadLines("input.txt")) {
+    var tokens = line.Split();
+    var amount = int.Parse(tokens[1]);
+    var direction = tokens[0];
+
+    switch (direction) {
+        case "forward":
+            x += amount;
+            break;
+        case "down":
+            y += amount;
+            break;
+        case "up":
+            y -= amount;
+            break;
+    }
+}
+
+Console.WriteLine(x * y);
