dac07e5b074dfea81ce56b9c3224cc90b54b4288
Jake Card <jacob.boxer@gmail.com>
2022-06-18 15:36:34 -0400

    Day 1

---
+++ Day01/Day01/Program.cs
@@ -0,0 +1,14 @@
+﻿var increases = 0;
+var lastDepth = int.MaxValue;
+
+foreach (var line in File.ReadLines("input.txt")) {
+    var currentDepth = int.Parse(line);
+
+    if (currentDepth > lastDepth) {
+        increases++;
+    }
+
+    lastDepth = currentDepth;
+}
+
+Console.WriteLine(increases);
