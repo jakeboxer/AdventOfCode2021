3e14d6635ada8ab25c241cddf2c39797084f7f0b
Jake Card <jacob.boxer@gmail.com>
2022-06-18 17:13:00 -0400

    Day 3 part 1

---
+++ Day02Part2/Day02Part2/Program.cs
@@ -0,0 +1,24 @@
+﻿var x = 0;
+var y = 0;
+var aim = 0;
+
+foreach (var line in File.ReadLines("input.txt")) {
+    var tokens = line.Split();
+    var amount = int.Parse(tokens[1]);
+    var direction = tokens[0];
+
+    switch (direction) {
+        case "forward":
+            x += amount;
+            y += aim * amount;
+            break;
+        case "down":
+            aim += amount;
+            break;
+        case "up":
+            aim -= amount;
+            break;
+    }
+}
+
+Console.WriteLine(x * y);
---
+++ Day03/Day03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+var nums = File.ReadLines("input.txt").ToArray();
+var halfCount = nums.Length * 0.5f;
+var size = nums[0].Length;
+var gamma = new StringBuilder();
+var epsilon = new StringBuilder();
+
+for (var digit = 0; digit < size; digit++) {
+    var count = 0;
+
+    foreach (var num in nums) {
+        if (num[digit] == '1') {
+            count++;
+        }
+    }
+
+    if (count >= halfCount) {
+        gamma.Append('1');
+        epsilon.Append('0');
+    } else {
+        gamma.Append('0');
+        epsilon.Append('1');
+    }
+}
+
+Console.WriteLine(Convert.ToInt32(gamma.ToString(), 2) * Convert.ToInt32(epsilon.ToString(), 2));
